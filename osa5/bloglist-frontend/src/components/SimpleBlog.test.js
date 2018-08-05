03764b3b33152a5f6de7f78f07c68dda7d1e3d12
makevertti <makevertti@users.noreply.github.com>
2018-08-05 13:07:19 +0300

    Osa 5: tehtävä 5.13

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import SimpleBlog from './SimpleBlog'
 
-describe.only('<SimpleBlog />', () => {
+describe('<SimpleBlog />', () => {
   it('renders content', () => {
     const blog = {
       title: 'test blog',
@@ -17,5 +17,23 @@ describe.only('<SimpleBlog />', () => {
     expect(titleAndAuthor.text()).toContain(blog.title)
     expect(titleAndAuthor.text()).toContain(blog.author)
     expect(likes.text()).toContain(blog.likes)
+  })
+
+  it('calls like buttons event handler function on press', () => {
+    const blog = {
+      title: 'test blog',
+      author: 'test blogger',
+      likes: 146
+    }
+
+    const mockHandler = jest.fn()
+
+    const simpleBlogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+    const button = simpleBlogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
